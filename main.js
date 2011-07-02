80d28d884b169aacf6d5ffdc173ec9da3866e4ea
Jing Wang <jingwang49@gmail.com>
2011-07-02 16:44:16 +1000

    1. added a new graph to see the turnover point of total repayment for normal and with offset account. 2. Adjusted the graph names and some result description phrasing.

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	calculateRepayment(loan_amount,offset_amount,loan_year,frequency,normal_interest,offset_interest);
 
 	$("#button1").click(function(){
-		$("#placeholder").fadeOut(1);
+		$("#placeholder1").fadeOut(1);
 		//update the form value
 		loan_amount=$("#loan_amount").val();
 		offset_amount=$("#offset_amount").val();
@@ -23,7 +23,7 @@ $(document).ready(function(){
 		offset_interest=$("#offset_interest").val();
 		//call the function again
 		calculateRepayment(loan_amount,offset_amount,loan_year,frequency,normal_interest,offset_interest);	
-		$("#placeholder").show(1000);
+		$("#placeholder1").show(1000);
 	});
 });
 
@@ -52,12 +52,12 @@ function calculateRepayment(loan_amount,offset_amount,loan_year,frequency,normal
 
 	file_put_contents('Result.json', $text);
 	*/
-	drawGraph(loan_amount,offset_amount,loan_year,normal_interest,offset_interest,frequency);
+	drawGraph1(loan_amount,offset_amount,loan_year,normal_interest,offset_interest,frequency);
+	drawGraph2(loan_amount,offset_amount,loan_year,normal_interest,offset_interest,frequency);
 	//echo "Your ".strtolower(frequency)." mortgage repayment<br /><h2>$".payment."</h2>";
 }
 					
-function drawGraph(ILA,OA,ML,NI,OI,frequency){
-	//Initial Loan Amount, Offset Amount, Mortgage Length, Normal Interest, Offset Interest
+function drawGraph1(initial_loan_amount,offset_amount,initial_loan_term,normal_interest,offset_interest,frequency){
 	var plot;
 	$(function(){
 		var period=0; 
@@ -70,92 +70,88 @@ function drawGraph(ILA,OA,ML,NI,OI,frequency){
 		else if (frequency=="Monthly"){
 			period=12;
 		}
-		var TNI=NI/(period*100); //Term Normal Interest
-		var TOI=OI/(period*100); //Term Offset Interest
-		var power=ML*period;
-		var TNP=ILA*TNI*Math.pow((1+TNI),power)/(Math.pow((1+TNI),power)-1);
-		var TOP=ILA*TOI*Math.pow((1+TOI),power)/(Math.pow((1+TOI),power)-1);
-		
+		var term_normal_interest = normal_interest/(period*100);
+		var term_offset_interest = offset_interest/(period*100);
+		var power = initial_loan_term*period;
+		var term_normal_payment = initial_loan_amount*term_normal_interest*Math.pow((1+term_normal_interest),power)/(Math.pow((1+term_normal_interest),power)-1);
+		var term_offset_payment = initial_loan_amount*term_offset_interest*Math.pow((1+term_offset_interest),power)/(Math.pow((1+term_offset_interest),power)-1);
 		//without offset
-		var LA1=[],IP=[],ALP1=[],result1=[]; //Loan Amount1,Interest Payment,Actual Loan Payment1
-		var N=ML*period;
+		var loan_amount1=[],interest_payment=[],actual_loan_payment1=[],result1=[];
+		var N=initial_loan_term*period;
 		for(var n=0;n<N;n++){
 			if(n==0){
-				IP.push(ILA*TNI);
+				interest_payment.push(initial_loan_amount*term_normal_interest);
 			}
 			else{
-				IP.push(LA1[n-1]*TNI);
+				interest_payment.push(loan_amount1[n-1]*term_normal_interest);
 			}
-			ALP1.push(TNP-IP[n]);
+			actual_loan_payment1.push(term_normal_payment-interest_payment[n]);
 			if(n==0){
-				LA1.push(ILA-ALP1[n]);
+				loan_amount1.push(initial_loan_amount-actual_loan_payment1[n]);
 			}
 			else{
-				LA1.push(LA1[n-1]-ALP1[n]);
+				loan_amount1.push(loan_amount1[n-1]-actual_loan_payment1[n]);
 			}
 		}
-		LA1[N-1]=0; 
+		loan_amount1[N-1]=0; 
 		
 		//with offset
-		var LA2=[],IA=[],AOIP=[],ALP2=[],result2=[]; //Loan Amount2,Interested Amount,After Offset Interest Payment,Actual Loan Payment2
+		var loan_amount2=[],interested_amount=[],after_offset_interest_payment=[],actual_loan_payment2=[],result2=[];
 		var n2=0;
-		while(n2==0||LA2[n2-1]>0){
+		while(n2==0||loan_amount2[n2-1]>0){
 			if(n2==0){
-				IA.push(ILA-OA); //alert("IA="+IA);
+				interested_amount.push(initial_loan_amount-offset_amount);
 			}
 			else{
-				IA.push(LA2[n2-1]-OA); //alert("IA="+IA);
+				interested_amount.push(loan_amount2[n2-1]-offset_amount);
 			}
-			if(IA[n2]<0){
-				IA[n2]=0;
+			if(interested_amount[n2]<0){
+				interested_amount[n2]=0;
 			}
-			AOIP.push(IA[n2]*TOI); //alert("AOIP="+AOIP);
-			ALP2.push(TOP-AOIP[n2]); //alert("ALP2="+ALP2);
+			after_offset_interest_payment.push(interested_amount[n2]*term_offset_interest);
+			actual_loan_payment2.push(term_offset_payment-after_offset_interest_payment[n2]);
 			if(n2==0){
-				LA2.push(ILA-ALP2[n2]); //alert("LA2="+LA2);
+				loan_amount2.push(initial_loan_amount-actual_loan_payment2[n2]);
 			}
 			else{
-				LA2.push(LA2[n2-1]-ALP2[n2]); //alert("LA2="+LA2);
+				loan_amount2.push(loan_amount2[n2-1]-actual_loan_payment2[n2]);
 			}
 			n2++;
 		}
-		var remainder = -1*LA2[n2-1];
-		var TP=[];
-		TP.push(N);
-		TP.push(TNP);
-		TP.push(n2);
-		TP.push(TOP);
-		TP.push(remainder);
-		LA2[n2]=0;
-		
+		var remainder = -1*loan_amount2[n2-1];
+		var account_result=[];
+		account_result.push(N);
+		account_result.push(term_normal_payment);
+		account_result.push(n2);
+		account_result.push(term_offset_payment);
+		account_result.push(remainder);
+		loan_amount2[n2]=0;
 		var i=0;
-		result1.push([0,ILA]);
+		result1.push([0,initial_loan_amount]); 
 		for(i=0;i<N;i++){
-			result1.push([(i+1)/period,LA1[i]]);
+			result1.push([(i+1)/period,loan_amount1[i]]);
 		}
-		
-		result2.push([0,ILA]);
+		result2.push([0,initial_loan_amount]);
 		for(i=0;i<n2;i++){
-			result2.push([(i+1)/period,LA2[i]]);
+			result2.push([(i+1)/period,loan_amount2[i]]);
 		}
-		
-		$("#_estimate_interest").html(function(){
-		return "Normal loan payment: $ <b><span class='_blue'>"+(Math.round(TP[1]*TP[0]*100)/100)+"</span></b><br />With offset loan payment:  $ <b><span class='_blue'>"+(Math.round(((TP[3]*TP[2])-TP[4])*100)/100)+"</span></b>";
+		$("#_estimate_interest1").html(function(){
+			return "Normal loan repayment: $ <b><span class='_blue'>"+(Math.round(account_result[1]*account_result[0]*100)/100)+"</span></b><br />With offset loan repayment:  $ <b><span class='_blue'>"+(Math.round(((account_result[3]*account_result[2])-account_result[4])*100)/100)+"</span></b>";
 		});
-		$("#_estimate_time").html(function(){
-		return "Estimate of normal loan term: <br /><b><span class='_blue'>"+(parseInt(TP[0]/period))+"</span></b> year(s) <b><span class='_blue'>"+Math.round((TP[0]%period)*12)+ "</span></b> month(s).<br />Estimate of with offset loan term: <br /><b><span class='_blue'>"+(parseInt(TP[2]/period))+"</span></b> year(s) <b><span class='_blue'>"+Math.round(((TP[2]/period)*12)%12)+ "</span></b> month(s).";
+		$("#_estimate_time1").html(function(){
+			return "Estimate of normal loan term: <br /><b><span class='_blue'>"+(parseInt(account_result[0]/period))+"</span></b> year(s) <b><span class='_blue'>"+Math.round((account_result[0]%period)*12)+ "</span></b> month(s).<br />Estimate of with offset loan term: <br /><b><span class='_blue'>"+(parseInt(account_result[2]/period))+"</span></b> year(s) <b><span class='_blue'>"+Math.round(((account_result[2]/period)*12)%12)+ "</span></b> month(s).";
 		});
-		$("#_estimate_difference").html(function(){
-		var payment_difference = Math.round(((TP[1]*TP[0])-((TP[3]*TP[2])-TP[4]))*100)/100;
-		var term_difference = Math.round((TP[0]-TP[2])/period*100)/100;
-		if(payment_difference<0){
-			return "Difference in payments: $ <b><span class='_red'>"+(-1*payment_difference)+"</span></b><br />Difference in loan terms: <b><span class='_green'>"+(parseInt(term_difference))+"</span></b> year(s) <b><span class='_green'>"+(Math.round(((term_difference)*12)%12))+"</span></b> month(s)";
-		}
-		else{
-			return "Difference in loan payments: $ <b><span class='_green'>"+(payment_difference)+"</span></b><br />Difference in loan terms: <b><span class='_green'>"+(parseInt(term_difference))+"</span></b> year(s) <b><span class='_green'>"+(Math.round(((term_difference)*12)%12))+"</span></b> month(s)";
-		}
+		$("#_estimate_difference1").html(function(){
+			var payment_difference = Math.round(((account_result[1]*account_result[0])-((account_result[3]*account_result[2])-account_result[4]))*100)/100;
+			var term_difference = Math.round((account_result[0]-account_result[2])/period*100)/100;
+			if(payment_difference<0){
+				return "Difference in loan repayments: $ <b><span class='_red'>"+(-1*payment_difference)+"</span></b><br />Difference in loan terms: <b><span class='_green'>"+(parseInt(term_difference))+"</span></b> year(s) <b><span class='_green'>"+(Math.round(((term_difference)*12)%12))+"</span></b> month(s)";
+			}
+			else{
+				return "Difference in loan repayments: $ <b><span class='_green'>"+(payment_difference)+"</span></b><br />Difference in loan terms: <b><span class='_green'>"+(parseInt(term_difference))+"</span></b> year(s) <b><span class='_green'>"+(Math.round(((term_difference)*12)%12))+"</span></b> month(s)";
+			}
 		});
-		plot=$.plot($("#placeholder"), 
+		plot=$.plot($("#placeholder1"), 
 		[ {
 			data: result1,
 		    color: "#06f206",
@@ -189,7 +185,7 @@ function drawGraph(ILA,OA,ML,NI,OI,frequency){
 			}
 		});
 		
-		var legends = $("#placeholder.legendLabel");
+		var legends = $("#placeholder1.legendLabel");
     	legends.each(function () {
 			// fix the widths so they don't jump around
 			$(this).css('width', $(this).width());
@@ -234,7 +230,160 @@ function drawGraph(ILA,OA,ML,NI,OI,frequency){
 						
 						
 						
-						$("#placeholder").bind("plothover", function (event, pos, item) {							
+						$("#placeholder1").bind("plothover", function (event, pos, item) {							
+							latestPosition = pos;
+							   /*if (item) {
+									  highlight(item.series, item.datapoint);
+									  alert("You clicked a point!");
+									}*/
+							if (!updateLegendTimeout)
+            					updateLegendTimeout = setTimeout(updateLegend, 50);
+						});
+						});
+						
+				}
+function drawGraph2(initial_loan_amount,initial_offset_amount,initial_loan_term,normal_interest,offset_interest,frequency){
+	var plot;
+	$(function(){
+		var period=0; 
+		if (frequency=="Weekly"){
+			period=52;
+		}
+		else if (frequency=="Fortnightly"){
+			period=26;
+		}
+		else if (frequency=="Monthly"){
+			period=12;
+		}
+		var term_normal_interest = normal_interest/(period*100);
+		var term_offset_interest = offset_interest/(period*100);
+		var power = initial_loan_term*period;
+		var term_normal_payment = initial_loan_amount*term_normal_interest*Math.pow((1+term_normal_interest),power)/(Math.pow((1+term_normal_interest),power)-1);
+		var term_offset_payment = initial_loan_amount*term_offset_interest*Math.pow((1+term_offset_interest),power)/(Math.pow((1+term_offset_interest),power)-1);
+		var min_amount=-1,min_id=0;
+		//without offset
+		var result1=[];
+		var N=initial_loan_term*period;
+		var total_normal_payment=N*term_normal_payment;
+		//with offset
+		var total_offset_payment=[],result2=[];
+		for(var offset_amount=0;offset_amount<=initial_loan_amount;offset_amount++){
+			var interested_amount=initial_loan_amount-offset_amount;
+			loan_term = (Math.log(term_offset_payment/(term_offset_payment-(interested_amount*term_offset_interest))))/(Math.log(1+(term_offset_interest)));
+			loan_term += offset_amount/term_offset_payment;
+			total_offset_payment.push(loan_term*term_offset_payment);
+			if((min_amount==-1)&&((total_normal_payment-total_offset_payment[offset_amount])>=0)){
+				min_amount=total_offset_payment[offset_amount];
+				min_id=offset_amount;
+			}
+		}
+		var scale = 10000;
+		for(var i=0;i<=parseInt(initial_loan_amount/scale);i++){
+			result1.push([(i*scale),total_normal_payment]);
+			result2.push([(i*scale),total_offset_payment[(i*scale)]]);
+		}
+		$("#_estimate_interest2").html(function(){
+			return "Normal loan amount: $ <b><span class='_blue'>"+initial_loan_amount+"</span></b><br />Normal loan repayment: $ <b><span class='_blue'>"+Math.round(total_normal_payment*100)/100+"</span></b>";
+		});
+		$("#_estimate_time2").html(function(){
+			return "Offset amount varies:<br />$ <b><span class='_blue'>0</span></b> ~ $ <b><span class='_blue'>"+initial_loan_amount+"</span></b><br />Offset loan repayment varies:<br />$ <b><span class='_blue'>"+Math.round(total_offset_payment[0]*100)/100+"</span></b> ~ $ <b><span class='_blue'>"+initial_loan_amount;
+		});
+		$("#_estimate_difference2").html(function(){
+			return "As long as there is more than $ <b><span class='_blue'>"+Math.round(min_amount*100)/100+"</span></b> in the offset account, the total repayment will be less than total repayment of normal loan.";
+		});
+		plot=$.plot($("#placeholder2"), 
+		[ {
+			data: result1,
+		    color: "#06f206",
+		},
+		{
+			data: result2,
+		    color:"#028802",
+		}
+		],   
+		{ 
+			series:{
+				lines: { show:true}
+			},
+		
+			crosshair: { mode: "x", lineWidth:10 },
+			grid: { hoverable:true, autoHighlight: false},
+			xaxis: {
+				min: 0, 
+				position:"bottom" ,
+				tickFormatter: function(val,axis){
+					if(val>=1000000)
+						return (val/1000000).toFixed(2)+"M";
+					else if(val==0)
+						return val;					
+					else if(val>1000&val<1000000)
+						return (val/1000).toFixed(0)+"K";
+					else
+						return val;									
+				}
+			},
+			yaxis: {
+				min: 0, 
+				position:"left" ,
+				tickFormatter: function(val,axis){
+					if(val>=1000000)
+						return (val/1000000).toFixed(2)+"M";
+					else if(val==0)
+						return val;					
+					else if(val>1000&val<1000000)
+						return (val/1000).toFixed(0)+"K";
+					else
+						return val;									
+				}
+			}
+		});
+		
+		var legends = $("#placeholder2.legendLabel");
+    	legends.each(function () {
+			// fix the widths so they don't jump around
+			$(this).css('width', $(this).width());
+		});						
+		var updateLegendTimeout = null;
+		var latestPosition = null;
+		
+    	function updateLegend() {
+
+        					updateLegendTimeout = null;
+        					var pos = latestPosition;
+        
+							var axes = plot.getAxes();
+
+        					//if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||pos.y < axes.yaxis.min || pos.y > axes.yaxis.max)
+
+            				//return;
+							
+							
+							var value1=result1[Math.round(pos.x)][1];
+							var value2=result2[Math.round(pos.x)][1];
+							var value3=mortgage-Math.round(pos.x);
+							
+							if (value2<=0||value1<=0){
+								value1=1;
+								value2=1;}
+							var test=value2/value1*100;
+							var value=value2/value1*250;
+							if (rtype=="Principal & Interest"){
+								$("#x").text(Math.round(test)+"% home laon");
+								$("#y").text(Math.round(100-test)+"% interest");
+								$("#_box").width(value);
+							}
+							else if(rtype=="Interest Only"){
+								$("#x").text("0% home loan");
+								$("#y").text("100% interest");
+								$("#_box").width(0);
+								}
+							$("#_remain_year").text(value3);
+						}
+						
+						
+						
+						
+						$("#placeholder2").bind("plothover", function (event, pos, item) {							
 							latestPosition = pos;
 							   /*if (item) {
 									  highlight(item.series, item.datapoint);
